2b0b81990d3251f44ac8f12966996b7e81d95c6c
Marcus <mackan1337.ml@gmail.com>
2020-12-16 16:23:30 +0100

    Update Gameboard.java

---
+++ src/model/Gameboard.java
@@ -0,0 +1,14 @@
+/*
+  Author: Marcus Linné, Linnéa Mörk
+  Id: ai8851, al2804
+  Study program: DT
+*/
+
+package model;
+
+public class gameboard {
+    Ship[][] board;
+    public gameboard(int size){
+        board = new Ship[size][size];
+    }
+}
